227dd6f7d2e5ff0f5ba7f9743c2ea5b0e9d0024a
Aluno <zottesso@hotmail.com>
2018-05-17 22:45:33 -0300

    Resolução de alguns exercícios com lista.

---
+++ aula15_exemplos.py
@@ -0,0 +1,68 @@
+# Pede o tamanho da lista
+t = int(input("Tamanho da lista:"))
+# Cria a lista com tamanho "t"
+lista = [""]*t
+
+# Preenchendo o vetor com valores do usuário
+p = 0
+while p < len(lista):
+    lista[p] = int(input("Número: "))
+    p+=1
+
+###############################################
+
+#
+# Fazendo a soma dos valores do vetor
+#
+soma = 0
+p = 0
+while p < len(lista):
+    # Acrescenta o valor da lista na posição p à soma
+    soma = soma + lista[p]
+    p+=1
+
+print("A soma da lista é",soma)
+print("A média da lista é", soma/len(lista) )
+
+#
+# Fazendo a média ponderada
+#
+soma = 0
+pesos = 0
+p = 0
+while p < len(lista):
+    # faz a multiplicação do valor * a posição dele
+    soma = soma + (lista[p] * p)
+    # soma += lista[p] * p
+
+    # Faz a soma dos pesos
+    pesos = pesos + p
+    # pesos += p
+
+    p+=1
+
+# Calcula a média
+medpon = soma / pesos
+print("Média ponderada:", medpon)
+
+#
+# Conta a quantidade de números pares e ímpares
+#
+par = 0
+impar = 0
+p = 0
+while p < len(lista):
+    
+
+    p += 1
+
+print("A quantidade de pares é", par)
+print("A quantidade de ímpares é", impar)
+
+
+
+
+
+
+
+
