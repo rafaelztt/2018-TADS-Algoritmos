88a9257101a5e40cf9a957fb8a9e926e786dda38
Aluno <rafael.zottesso@ifpr.edu.br>
2018-06-07 21:21:00 -0300

    Mais melhorias no código.

--- git.py
+++ git.py
@@ -12,10 +12,14 @@ email = input("Digite enter para manter o {} como padrão ou informe seu e-mail:
 if email == "":
     email = email_padrao
 
+print("Adicionando os novos arquivos...\n")
 os.system("git add *")
 
+print("Configurando o email do usuário...\n")
 os.system('git config user.email "'+email+'"')
 
+print("Efetuando o commit das modificações...\n")
 os.system('git commit -m "'+msg_commit+'"')
 
+print("Conectando com os servidores do Github...\n")
 os.system('git push origin master')
