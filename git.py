bca934141fe9b38ba23feedae4b1d43acd1f8595
Aluno <rafael.zottesso@ifpr.edu.br>
2018-06-07 21:18:46 -0300

    Melhorias no código.

--- git.py
+++ git.py
@@ -1,10 +1,16 @@
 import os
 
 msg_commit = input("Mensagem do commit: ")
-email = input("Digite enter para manter o rafael.zottesso@ifpr.edu.br como padrão ou informe seu e-mail: ")
+
+while len(msg_commit) < 5:
+    print("A mensagem deve ter pelo menos 5 caracteres.")
+    msg_commit = input("Mensagem do commit: ")
+
+email_padrao = "rafael.zottesso@ifpr.edu.br"
+email = input("Digite enter para manter o {} como padrão ou informe seu e-mail: ".format(email_padrao))
 
 if email == "":
-    email = "rafael.zottesso@ifpr.edu.br"
+    email = email_padrao
 
 os.system("git add *")
 
