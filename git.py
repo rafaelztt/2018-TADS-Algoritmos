8748a3b5433355d8c0b216737215fc8f0638d129
Aluno <rafael.zottesso@ifpr.edu.br>
2018-06-07 21:14:59 -0300

    bla bla bla

--- git.py
+++ git.py
@@ -1,9 +1,15 @@
 import os
 
-msg_commit = "Adição dos comandos git."
+msg_commit = input("Mensagem do commit: ")
+email = input("Digite enter para manter o rafael.zottesso@ifpr.edu.br como padrão ou informe seu e-mail: ")
+
+if email == "":
+    email = "rafael.zottesso@ifpr.edu.br"
 
 os.system("git add *")
 
-os.system('git config user.email "rafael.zottesso@ifpr.edu.br"')
+os.system('git config user.email "'+email+'"')
 
 os.system('git commit -m "'+msg_commit+'"')
+
+os.system('git push origin master')
