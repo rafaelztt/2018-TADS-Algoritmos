2abf85982b584e1df99635a4d4df3dcd8ea08131
Aluno <rafael.zottesso@ifpr.edu.br>
2018-06-14 22:32:13 -0300

    Função para gerar números aleatórios.

--- aula17.py
+++ aula17.py
@@ -1,25 +1,34 @@
 # Pede o tamanho da lista
-t = int(input("Tamanho da lista:"))
+# t = int(input("Tamanho da lista:"))
 # Cria a lista com tamanho "t"
-lista = [""]*t
+# lista = [""]*t
 
 # Preenchendo o vetor com valores do usuário
-p = 0
-while p < len(lista):
-    lista[p] = int(input("Número: "))
-    p+=1
+# p = 0
+# while p < len(lista):
+#     lista[p] = int(input("Número: "))
+#     p+=1
 # Fim do preenchimento
 
 # "Desenha" o vetor na tela
-print(lista)
+# print(lista)
 
 # Usando um laço de repetição for
 # for nomeDaVariavel in nomeDaLista:
 # A variável escolhida vai assumir os valores da lista.
 # A cada rodada ele assume o valor conforme a ordem que o vetor foi criado
-for valor in lista:
-    dobro = valor * 2
-    print("Valor armazenado: {} e o seu dobro: {}".format(valor, dobro) )
+# for valor in lista:
+#     dobro = valor * 2
+#     print("Valor armazenado: {} e o seu dobro: {}".format(valor, dobro) )
 
 # Mostrando que a lista não é alterada
-print(lista)
+# print(lista)
+
+# Gerando número aleatório
+#   biblioteca      função
+from random import randint
+
+# Informe o número inicial e o final
+x = randint(0, 9)
+
+print(x)
