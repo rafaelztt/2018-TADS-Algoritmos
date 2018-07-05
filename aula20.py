75ff7ed318949d57ac9f216a8ee50ec488fbeeb7
Aluno <rafael.zottesso@ifpr.edu.br>
2018-07-05 20:17:20 -0300

    Introdução a criação de funções simples.

---
+++ aula20.py
@@ -0,0 +1,44 @@
+# É obrigatório ter parênteses junto com o nome da função
+# e não deve ter espaço entre o nome e os parênteses
+# Para criar uma função basta seguir o exemplo abaixo:
+# def nomeDaFuncao():
+#   Código da função com tab
+#
+def mostrarErro1():
+    # Código da função
+    print("""
+_____
+|    |
+|    O
+|
+|    """)
+
+# Aqui a tabulação já finaliza a função anterior
+def mostrarErro2():
+    print("""
+_____
+|    |
+|    O
+|   /
+|    """)
+
+
+########## Início do programa ############
+
+erro=2
+Lá pela linha 50
+"""
+...
+Seu código
+...
+"""
+
+if erro == 1:
+    # Coloque o nomeDaFuncao() e ela será executada aqui
+    mostrarErro1() # parênteses são obrigatórios com o nome das funções
+    # continuação
+    # ...
+elif erro == 2:
+    mostrarErro2()
+    # continuação
+    # ...
