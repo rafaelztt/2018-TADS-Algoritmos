1bdeace026996f16f513d56e7de33805d8c775f1
Aluno <rafael.zottesso@ifpr.edu.br>
2018-08-02 22:27:21 -0300

    Funções com retorno e/ou parâmetros.

--- aula22.py
+++ aula22.py
@@ -32,5 +32,82 @@ def exemplo1():
         print("Acesso negado!")
 
 
+# Função sem retorno e sem parâmetro para calcular a tabuada
+def calc_tabuada():
+    n = int(input("Número: "))
+
+    print("Calculando tabuada...")
+
+    a = 1
+    while a <= 10:
+        print(a,"*",n,'=',a*n)
+        a+=1
+
+# Exemplo de função com parâmetro e sem retorno
+def exemplo2(num):
+
+    print("O número informado é:", num)
+
+    if num % 2 == 0 :
+        print("Este número é par!")
+    else:
+        print("Número ímpar detectado!")
+
+# Exemplo de função com parâmetro e sem retorno
+# Funções podem ter o mesmo nome, porém com quantidade
+# diferente de parâmetros
+def calc_tabuada(n):
+
+    #o range pode ter o valor inicial e final da contagem
+    for x in range(1, 11):
+        print(n,"*",x,'=',x*n)
+
+
+# Exemplo de função com parâmetro e com retorno
+def exemplo3(a):
+
+    triplo = a * 3
+
+    # Retorna o valor da variável abaixo
+    return triplo
+
+def exemplo4(num):
+
+    if num % 2 == 0:
+        # Pode colocar uma operação no return
+        # pois ela será resolvida antes de enviar o resultado
+        return num**2
+    else :
+        n = num ** 3
+        # Podemos também devolver o valor de uma variável
+        return n
+
 ##### Fim das funções ####
-exemplo1()
+# Chama a função para ser executada
+# exemplo1()
+
+# Chamada de função
+# calc_tabuada()
+
+# Chamada de função
+# a = 10
+# exemplo2(a)
+
+# b = int( input("Informe um número:") )
+# exemplo2(b)
+
+# Chamda de função com parâmetros
+# calc_tabuada(8)
+
+# b = int( input("Informe um número:") )
+# calc_tabuada(b)
+
+# Chamada de função com retorno
+k = exemplo4(852)
+print("Resposta:", k)
+
+# O retorno de uma função pode ser utilizado na chamada de outra função
+calc_tabuada(k)
+
+# Funções com retorno podem ser usadas direto no print
+print("Resposta direta:", exemplo4(750))
